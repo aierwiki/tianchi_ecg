f21e417ddaf452ead84ec1f91890129628c4371f
Your Name <you@example.com>
2019-10-10 23:46:47 +0800

    add other file

---
+++ project/data/download.sh
@@ -0,0 +1,10 @@
+wget http://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/231754/round1/hf_round1_train.zip
+wget http://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/231754/round1/hf_round1_label.txt
+wget http://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/231754/round1/hf_round1_arrythmia.txt
+wget http://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/231754/round1/hf_round1_testA.zip
+wget http://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/231754/round1/hf_round1_testB_noDup_rename.zip
+wget http://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/231754/round1/hf_round1_subB_noDup_rename.txt
+unzip http://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/231754/round1/hf_round1_train.zip
+unzip http://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/231754/round1/hf_round1_testA.zip
+unzip http://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/231754/round1/hf_round1_testB_noDup_rename.zip
+mv hefei_round1_ansA_20191008.txt.tmp hefei_round1_ansA_20191008.txt
